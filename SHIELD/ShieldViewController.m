69b81173aa12f5040b26ad158f39939bb34b24f4
Pavel Gurov <pashaisthebest@hotmail.com>
2014-08-18 23:23:43 +0400

    ShieldViewController progress

--- SHIELD/ShieldViewController.m
+++ SHIELD/ShieldViewController.m
@@ -9,41 +9,87 @@
 #import "ShieldViewController.h"
 
 @interface ShieldViewController ()
+@property (nonatomic) CGPoint initialPanCenter;
+@property (nonatomic) CGPoint currentPanCenter;
 
+@property (strong, nonatomic) IBOutlet UIPanGestureRecognizer *panGR;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageViewEllipse;
+- (IBAction)viewPanned:(UIPanGestureRecognizer *)sender;
 @end
 
 @implementation ShieldViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self showEllipse:NO animated:NO];
 }
 
-- (void)didReceiveMemoryWarning
+//---------------------------------------------------------------------------
+#pragma mark - Setters
+
+- (void)setCurrentPanCenter:(CGPoint)currentPanCenter
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    _currentPanCenter = currentPanCenter;
+    
+    // adjust position of the ellipse
+    CGPoint ellipseCenter = self.imageViewEllipse.center;
+    ellipseCenter.y = currentPanCenter.y;
+    [self.imageViewEllipse setCenter:ellipseCenter];
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+//---------------------------------------------------------------------------
+#pragma mark - User actions
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    UITouch *someTouch = [touches anyObject];
+    
+    if (someTouch) {
+        self.currentPanCenter = [someTouch locationInView:self.view];
+        [self showEllipse:YES animated:YES];
+    }
 }
-*/
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self showEllipse:NO animated:YES];
+}
+
+- (IBAction)viewPanned:(UIPanGestureRecognizer *)sender
+{
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        
+        self.initialPanCenter = [sender locationInView:self.view];
+        [self showEllipse:YES animated:YES];
+    }
+    
+    if (sender.state == UIGestureRecognizerStateEnded ||
+        sender.state == UIGestureRecognizerStateCancelled) {
+        
+        [self showEllipse:NO animated:YES];
+    }
+    
+    CGFloat translationY = [sender translationInView:self.view].y;
+    CGFloat translationX = [sender translationInView:self.view].x;
+    self.currentPanCenter = CGPointMake(self.initialPanCenter.x + translationX, self.initialPanCenter.y + translationY);
+}
+
+//---------------------------------------------------------------------------
+#pragma mark - Helpers
+
+// pass YES to show, NO to hide
+- (void)showEllipse:(BOOL)show animated:(BOOL)animated
+{
+    CGFloat duration = animated ? 0.1 : 0;
+    CGFloat alpha = show? 1 : 0;
+    
+    [UIView animateWithDuration:duration animations:^{
+        self.imageViewEllipse.alpha = alpha;
+    }];
+}
 
 @end
