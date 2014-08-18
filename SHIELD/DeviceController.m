9096f9f888dbc9617d8be2988ade6af09b7a5e9d
Andrey Ogrenich <andrey.ogrenich@gmail.com>
2014-08-18 17:26:26 +0400

    Добавлен DeviceController

---
+++ SHIELD/DeviceController.m
@@ -0,0 +1,29 @@
+//
+//  DeviceController.m
+//  Tester
+//
+//  Created by Andrey Ogrenich on 29/07/14.
+//  Copyright (c) 2014 Andrey Ogrenich. All rights reserved.
+//
+
+#import "DeviceController.h"
+#import <CoreBluetooth/CoreBluetooth.h>
+#import "BTManager.h"
+
+@interface DeviceController ()
+
+@end
+
+@implementation DeviceController {
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UInt8 buf[1] = {0x01};
+    NSData *data = [[NSData alloc] initWithBytes:buf length:1];
+    [[BTManager sharedInstance] writeValue:data];
+}
+
+@end
