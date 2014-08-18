8d22800b417a9ef5368619f4b3fd5e4cb9021bd3
Andrey Ogrenich <andrey.ogrenich@gmail.com>
2014-08-18 13:06:33 +0400

    Initial Commit

---
+++ SHIELD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHIELD
+//
+//  Created by Andrey Ogrenich on 18.08.14.
+//  Copyright (c) 2014 Andrey Ogrenich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SHIELD/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SHIELD
+//
+//  Created by Andrey Ogrenich on 18.08.14.
+//  Copyright (c) 2014 Andrey Ogrenich. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
