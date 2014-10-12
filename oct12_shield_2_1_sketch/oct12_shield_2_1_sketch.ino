39e975f8a4429886adbc81f65703e963a810df59
Pavel Gurov <pashaisthebest@hotmail.com>
2014-10-12 18:21:26 +0400

    save

--- oct12_shield_2_1_sketch/oct12_shield_2_1_sketch.ino
+++ oct12_shield_2_1_sketch/oct12_shield_2_1_sketch.ino
@@ -1,29 +1,34 @@
 #include <SoftwareSerial.h>   // Модуль для работы с Serial
- 
+
 SoftwareSerial mySerial(0, 1);
 
 /* 
-SHIELD Controller v2.1 Frimware
-
-Аналоговые PIN:
-А0 — напряжение заряда аккумулятора №1
-А1 — напряжение заряда аккумулятора №2
-А2 — напряжение заряда аккумулятора №3
-А3 — напряжение заряда аккумулятора №4
-
-Входные напряжение этих пинов надо будет подбирать по результатам испытаний, когда будет скетч готов, ориентировочно:
-3.6V — зарядка 0%
-4.0V — зарядка 100%
-
-Общая зарядка аккумуляторов будет считаться как среднее арифметическое 4-х значений. А контроль каждого аккумулятора нам необходим т.к. они разряжаются неравномерно.
-А4 — сигнал "подключена зарядка" если зарядка подключена приходит напряжение (4V, если не подключена, то напряжение на входе "0").
-
-Цифровые PIN:
-10 — управление логотипом
-12 — управление нагревом
-*/
+ SHIELD Controller v2.1 Frimware
+ 
+ Аналоговые PIN:
+ А0 — напряжение заряда аккумулятора №1
+ А1 — напряжение заряда аккумулятора №2
+ А2 — напряжение заряда аккумулятора №3
+ А3 — напряжение заряда аккумулятора №4
+ 
+ Входные напряжение этих пинов надо будет подбирать по результатам испытаний, когда будет скетч готов, ориентировочно:
+ 3.6V — зарядка 0%
+ 4.0V — зарядка 100%
+ 
+ Общая зарядка аккумуляторов будет считаться как среднее арифметическое 4-х значений. А контроль каждого аккумулятора нам необходим т.к. они разряжаются неравномерно.
+ А4 — сигнал "подключена зарядка" если зарядка подключена приходит напряжение (4V, если не подключена, то напряжение на входе "0").
+ 
+ Цифровые PIN:
+ 10 — управление логотипом
+ 12 — управление нагревом
+ */
 
 // Объявляем переменные
+const int CHANGING_HEAT_LEVEL = 102;
+
+int whatWereDoingByte = 255;
+int valueByte = 255;
+
 int logo = 10; // Логотип
 int heat = 12; // Нагревательные элементы
 int charge = 13; // Индикатор зарядки SHIELD от внешней сети
@@ -67,6 +72,7 @@ const float Charging_Ratio = Charging_Coefficient * Charging_Coefficient_Ratio;
 
 // Инициализация переменных
 void setup() {
+
   Serial.begin(115200);
   mySerial.begin(115200);
   pinMode(logo, OUTPUT);
@@ -81,97 +87,134 @@ void setup() {
 
 // Основной цикл программы
 void loop() {
-  
-  /* BATTERIES
-  Battery0_Value = analogRead(Battery0);
-  Battery1_Value = analogRead(Battery1);
-  Battery2_Value = analogRead(Battery2);
-  Battery3_Value = analogRead(Battery3);
-  
-  // Расчёт напряжения в вольтах
-  Battery0_Voltage = Battery0_Value*Battery_Ratio;
-  Battery1_Voltage = Battery1_Value*Battery_Ratio;
-  Battery2_Voltage = Battery2_Value*Battery_Ratio;
-  Battery3_Voltage = Battery3_Value*Battery_Ratio;
-  
-  // Выводим напряжение батарей в консоль
-  Serial.print("BATTERY 0 VOLTAGE = ");
-  Serial.println(Battery0_Voltage);
-  Serial.print("BATTERY 1 VOLTAGE = ");
-  Serial.println(Battery1_Voltage);
-  Serial.print("BATTERY 2 VOLTAGE = ");
-  Serial.println(Battery2_Voltage);
-  Serial.print("BATTERY 3 VOLTAGE = ");
-  Serial.println(Battery3_Voltage);
 
-  // Если напряжение на какой-либо из батарей падает до 3.55 V мы отключаем нагрев
-  if (Battery0_Voltage <= 3.55) {
+  /* BATTERIES
+   Battery0_Value = analogRead(Battery0);
+   Battery1_Value = analogRead(Battery1);
+   Battery2_Value = analogRead(Battery2);
+   Battery3_Value = analogRead(Battery3);
+   
+   // Расчёт напряжения в вольтах
+   Battery0_Voltage = Battery0_Value*Battery_Ratio;
+   Battery1_Voltage = Battery1_Value*Battery_Ratio;
+   Battery2_Voltage = Battery2_Value*Battery_Ratio;
+   Battery3_Voltage = Battery3_Value*Battery_Ratio;
+   
+   // Выводим напряжение батарей в консоль
+   Serial.print("BATTERY 0 VOLTAGE = ");
+   Serial.println(Battery0_Voltage);
+   Serial.print("BATTERY 1 VOLTAGE = ");
+   Serial.println(Battery1_Voltage);
+   Serial.print("BATTERY 2 VOLTAGE = ");
+   Serial.println(Battery2_Voltage);
+   Serial.print("BATTERY 3 VOLTAGE = ");
+   Serial.println(Battery3_Voltage);
+   
+   // Если напряжение на какой-либо из батарей падает до 3.55 V мы отключаем нагрев
+   if (Battery0_Voltage <= 3.55) {
    digitalWrite(heat, LOW);
    Serial.println("SHIELD IS TURNED OFF, BATTERY 0 VOLTAGE <= 3.55 V");
-  } else if (Battery1_Voltage <= 3.55) {
+   } else if (Battery1_Voltage <= 3.55) {
    digitalWrite(heat, LOW);
    Serial.println("SHIELD IS TURNED OFF, BATTERY 1 VOLTAGE <= 3.55 V");
-  } else if (Battery2_Voltage <= 3.55) {
+   } else if (Battery2_Voltage <= 3.55) {
    digitalWrite(heat, LOW);
    Serial.println("SHIELD IS TURNED OFF, BATTERY 2 VOLTAGE <= 3.55 V");
-  } else if (Battery3_Voltage <= 3.55) {
+   } else if (Battery3_Voltage <= 3.55) {
    digitalWrite(heat, LOW);
    Serial.println("SHIELD IS TURNED OFF, BATTERY 3 VOLTAGE <= 3.55 V ");
-  } */
-  
-  /* CHARGING
-  Charging_Value = analogRead(Charging);
-  
-  // Расчёт в вольтах
-  Charging_Voltage = Charging_Value*Charging_Ratio;
-  Serial.print("CHARGING VOLTAGE = ");
-  Serial.println(Charging_Voltage);
-  
-  
-  // Если напряжение на входе меньше 5V - зарядное устройство не подключено
-  if (Charging_Voltage < 5) {
-    digitalWrite(charge, LOW);
-  } else {
-    digitalWrite(charge, HIGH);
-  } */
+   } */
 
-  
+  /* CHARGING
+   Charging_Value = analogRead(Charging);
+   
+   // Расчёт в вольтах
+   Charging_Voltage = Charging_Value*Charging_Ratio;
+   Serial.print("CHARGING VOLTAGE = ");
+   Serial.println(Charging_Voltage);
+   
+   
+   // Если напряжение на входе меньше 5V - зарядное устройство не подключено
+   if (Charging_Voltage < 5) {
+   digitalWrite(charge, LOW);
+   } else {
+   digitalWrite(charge, HIGH);
+   } */
+
+
   /* TWIX
-  delay(10000); */
-  
-  
+   delay(10000); */
 
   /* SWITCHING HANDMODE
-  if (Serial.available() > 0){
-    incomingbyte = Serial.read();
-      if (incomingbyte == '1') {
-        digitalWrite(logo, HIGH);
-        digitalWrite(heat, HIGH);
-        Serial.println("SHIELD IS TURNED ON");
-      } else {
-        digitalWrite(logo, LOW);
-        digitalWrite(heat, LOW);
-        Serial.println("SHIELD IS SWITCHED OFF");
-      }
-  } */
-  
+   if (Serial.available() > 0){
+   incomingbyte = Serial.read();
+   if (incomingbyte == '1') {
+   digitalWrite(logo, HIGH);
+   digitalWrite(heat, HIGH);
+   Serial.println("SHIELD IS TURNED ON");
+   } else {
+   digitalWrite(logo, LOW);
+   digitalWrite(heat, LOW);
+   Serial.println("SHIELD IS SWITCHED OFF");
+   }
+   } */
+
   /* SWITCHING BLE */
   if(mySerial.available() > 0) {
-     incomingbyte = mySerial.read();
-     Serial.println(incomingbyte);
-      if(incomingbyte == 49) {
-        if (c % 2 != 0) {
-          digitalWrite(logo, HIGH);
-          digitalWrite(heat, HIGH);
-          Serial.println("SHIELD IS TURNED ON");
-          c = c + 1;
-        } else {
-          digitalWrite(logo, LOW);
-          digitalWrite(heat, LOW);
-          Serial.println("SHIELD IS SWITCHED OFF");
-          c = c + 1;
-        }
+
+    // int currentMessagePointer = 0;
+    // int whatWereDoingByte = 0;
+    // int valueByte = 0;
+
+    incomingbyte = mySerial.read();
+    Serial.print("GOT BYTE: ");
+    Serial.println(incomingbyte);
+
+    if (incomingbyte == CHANGING_HEAT_LEVEL) { // what were doing byte
+      whatWereDoingByte = incomingbyte;
+    }
+    else if (incomingbyte >= 0 && incomingbyte <= 100) { // value byte
+      valueByte = incomingbyte;
+    }
+    else {
+      valueByte = 255;        
+      whatWereDoingByte = 255;
+    }
+
+    if (whatWereDoingByte != 255 && valueByte !=255) {
+
+      // decide on what to do with the incoming data
+      if (whatWereDoingByte == CHANGING_HEAT_LEVEL) {
+        setHeatVelueToShield(valueByte);
       }
+
+      valueByte = 255;        
+      whatWereDoingByte = 255;
+    }
   } 
-  
 }
+
+int currentShieldHeatValue = 0;
+
+void setHeatVelueToShield( int value ) {
+
+  if (currentShieldHeatValue == 0 && value!=0) {
+    // turning on
+    digitalWrite(logo, HIGH);
+    digitalWrite(heat, HIGH);
+    Serial.println("SHIELD IS SWITCHING ON");
+  }
+  else if (currentShieldHeatValue != 0 && value == 0) {
+    digitalWrite(logo, LOW);
+    digitalWrite(heat, LOW);
+    Serial.println("SHIELD IS SWITCHING OFF");
+  }
+
+  currentShieldHeatValue = value;
+  Serial.print("SETTING VALUE TO SHIELD: ");
+  Serial.println(currentShieldHeatValue);
+
+  //    here is the place to put the code that physically controls shield
+
+}
+
