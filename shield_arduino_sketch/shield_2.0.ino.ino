71fec84729db1f54763a0aa3cfc91b847b312ff2
igorzelenov <igor.zelenov@gmail.com>
2015-03-28 21:21:38 +0300

    New sketch! 2.0

---
+++ shield_arduino_sketch/shield_2.0.ino.ino
@@ -0,0 +1,58 @@
+#include <SoftwareSerial.h>
+
+int logo = 9;
+int heat = 10;
+
+SoftwareSerial BLE(0, 1);
+
+const int CHANGING_HEAT_LEVEL = 101;
+
+int whatWereDoingByte = 255;
+int valueByte = 255;
+
+void setup() {
+  Serial.begin(115200);  
+  BLE.begin(115200);
+}
+
+void loop() {  
+  
+  char recvChar;
+  while (BLE.available()>0) {
+    
+    if( BLE.available() ){ //check if there's any data sent from the remote BLE shield
+      byte recvByte = (byte)BLE.read();
+      
+      if (recvByte == CHANGING_HEAT_LEVEL) { // changing heat level byte
+        whatWereDoingByte = recvByte;
+      }
+      else if (recvByte >= 0 && recvByte <= 100) { // value byte
+        valueByte = recvByte;
+      }
+      else {
+        valueByte = 255;        
+        whatWereDoingByte = 255;
+      }      
+    }
+  }
+  
+  // check everything we got
+  if (whatWereDoingByte != 255 && valueByte !=255) {
+
+    // decide on what to do with the incoming data
+    if (whatWereDoingByte == CHANGING_HEAT_LEVEL) {
+      setHeatVelueToShield(valueByte);
+    }
+
+    valueByte = 255;        
+    whatWereDoingByte = 255;
+  }
+  
+}
+
+void setHeatVelueToShield( int value ) {
+  analogWrite(heat, map(value, 0, 100, 0, 255));
+  analogWrite(logo, map(value, 0, 100, 0, 255));
+}
+
+
